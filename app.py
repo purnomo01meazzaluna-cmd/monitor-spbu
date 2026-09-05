834cdd269849775e2a74c7d0b4961f136b0fa382
purnomo01meazzaluna-cmd <purnomo01meazzaluna@gmail.com>
2026-09-05 08:20:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 
 # Page Configuration
 st.set_page_config(
-    page_title="Monitor Subsidi Tepat Guna - SPBU",
+    page_title="Monitoring Subsidi Tepat  - SPBU TAC ",
     page_icon="⛽",
     layout="wide",
     initial_sidebar_state="expanded"
