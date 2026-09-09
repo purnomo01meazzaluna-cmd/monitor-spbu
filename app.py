fd5e8a92b3c91bbb097f6e54f044ce6a013669f1
purnomo01meazzaluna-cmd <purnomo01meazzaluna@gmail.com>
2026-09-09 19:48:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,14 +29,14 @@ pilihan_kategori = st.sidebar.selectbox(
 # Definisi rentang sub-kategori berdasarkan aturan yang diberikan
 sub_kategori_rules = {
     "JBT-Solar": [
-        {"nama": "1. 1000-2999 R4 Pribadi", "min": 1000, "max": 2999},
+        {"nama": "🚗 1000-2999 R4 Pribadi", "min": 1000, "max": 2999},
         {"nama": "2. 3000-6999 R2 SPM", "min": 3000, "max": 6999},
         {"nama": "3. 7000-7999 R4 Umum Bus", "min": 7000, "max": 7999},
         {"nama": "4. 8000-8999 R4> B Truck Barang", "min": 8000, "max": 8999},
         {"nama": "5. 9000-9999 R4> Truck Khusus", "min": 9000, "max": 9999},
     ],
     "JBKP-Pertalite": [
-        {"nama": "1. 1000-2999 R4 Pribadi", "min": 1000, "max": 2999},
+        {"nama": "🚗 1000-2999 R4 Pribadi", "min": 1000, "max": 2999},
         {"nama": "2. 3000-6999 R2 SPM", "min": 3000, "max": 6999},
         {"nama": "3. 7000-7999 R4 Umum Bus", "min": 7000, "max": 7999},
         {"nama": "4. 8000-8999 R4 B Pick Up Barang", "min": 8000, "max": 8999},
